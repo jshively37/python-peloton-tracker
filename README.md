6104f02d7bb22ccb57e08a662098c528ac994102
Jeffrey <jeffrey.shively@hashicorp.com>
2022-07-31 11:35:20 -0400

    Initial push

---
+++ README.md
@@ -0,0 +1,15 @@
+# Peloton Tracker
+
+**Work In Progress**
+
+This project will interact with the Peloton API to retrieve data.
+
+MVP project is focused on graphing the amount of miles ridden in a specific period of time.
+
+## Requirements
+
+## Consuming
+
+## Contributing
+
+## License
---
+++ main.py
@@ -0,0 +1,51 @@
+import json
+import os
+from urllib.error import HTTPError
+
+# Ensure requests is installed
+try:
+    import requests
+except ImportError:
+    print("requests not found")
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+    PELOTON_PATH = os.environ.get("PELOTON_PATH")
+    PELOTON_USERNAME = os.environ.get("PELOTON_USERNAME")
+    PELOTON_PASSWORD = os.environ.get("PELOTON_PASSWORD")
+except ImportError:
+    print("python-dotenv not found")
+
+# Dictionary of Peloton API Endpoints
+PELOTON_SLUGS = {
+    "auth": "/auth/login"
+}
+
+# Headers for interacting with Peloton API
+PELOTON_HEADERS = {
+
+}
+
+
+def api_request(slug, headers=PELOTON_HEADERS, payload=None, method="GET"):
+    url = PELOTON_PATH + slug
+    response = requests.request(method, url, data=payload, headers=headers)
+    try:
+        response.raise_for_status()
+        return response.json()
+    except HTTPError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    payload = json.dumps({
+        "username_or_email": f"{PELOTON_USERNAME}",
+        "password": f"{PELOTON_PASSWORD}"
+        })
+    userid = api_request(
+        PELOTON_SLUGS['auth'],
+        PELOTON_HEADERS,
+        payload=payload,
+        method="POST")
+    print(userid['user_id'])
