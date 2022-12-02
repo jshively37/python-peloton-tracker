5953bad810f903a976a19be290bda9a595fd7036
Jeffrey <jeffrey.shively@hashicorp.com>
2022-12-02 09:55:44 -0500

    Passing flake8

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from dotenv import load_dotenv
 
@@ -10,11 +11,31 @@ PELOTON_USERNAME = os.environ.get("PELOTON_USERNAME")
 PELOTON_PASSWORD = os.environ.get("PELOTON_PASSWORD")
 
 
+# Need to determine why some workouts the time_seconds field is null but
+# data exists in the payload.
+def parse_workouts(workouts):
+    for workout in workouts:
+        try:
+            if (
+                workout["fitness_discipline"] == "cycling"
+                and workout["v2_total_video_watch_time_seconds"] >= 600
+            ):
+                date = get_date(workout)
+                print(date)
+        except TypeError:
+            print(workout)
+
+
+def get_date(workout):
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(workout["created_at"]))
+
+
 if __name__ == "__main__":
     client = PelotonClient(
         username=PELOTON_USERNAME, password=PELOTON_PASSWORD, url_path=PELOTON_PATH
     )
     client.create_session()
     all_workouts = client.get_all_workouts()
-    print(all_workouts)
+    parse_workouts(all_workouts)
+
     print(len(all_workouts))
