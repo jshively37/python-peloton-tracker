93e337f37be999c8a9a060ca9ba989591c3f5ab0
Jeffrey <jeffrey.shively@hashicorp.com>
2022-12-02 10:36:25 -0500

    MVP of getting PR information and parsing the data

--- main.py
+++ main.py
@@ -4,10 +4,29 @@ from dotenv import load_dotenv
 
 from peloton.client import PelotonClient
 
+
 load_dotenv()
 PELOTON_PATH = os.environ.get("PELOTON_PATH")
 PELOTON_USERNAME = os.environ.get("PELOTON_USERNAME")
 PELOTON_PASSWORD = os.environ.get("PELOTON_PASSWORD")
+
+
+def parse_pr_workouts(workout):
+    pr_dict_values = {}
+    response = client.get_workout_detail(workout['id'])
+    for summary in response['summaries']:
+        if summary['display_name'].lower() == "distance":
+            miles = summary['value']
+            pr_dict_values["miles"] = miles
+        elif summary['display_name'].lower() == "calories":
+            calories = summary['value']
+            pr_dict_values["calories"] = calories
+        elif summary['display_name'].lower() == "total output":
+            output = summary['value']
+            pr_dict_values["output"] = output
+    pr_data = {response['duration'] / 60: pr_dict_values}
+    print(pr_data)
+    print('-'*100)
 
 
 if __name__ == "__main__":
@@ -16,5 +35,8 @@ if __name__ == "__main__":
     )
     client.create_session()
     all_workouts = client.get_all_workouts()
-    print(all_workouts)
-    print(len(all_workouts))
+
+    pr_dict = {}
+    for workout in all_workouts:
+        if workout['is_total_work_personal_record']:
+            parse_pr_workouts(workout)
