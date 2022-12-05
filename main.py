5b92639a67743bbc3050fd31e25fa6c6c180e7ba
Jeffrey <jeffrey.shively@hashicorp.com>
2022-12-05 11:15:23 -0500

    Adding in documentation for functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import typing as t
 
 from dotenv import load_dotenv
 
@@ -12,7 +13,15 @@ PELOTON_USERNAME = os.environ.get("PELOTON_USERNAME")
 PELOTON_PASSWORD = os.environ.get("PELOTON_PASSWORD")
 
 
-def parse_pr_workout(workout):
+def parse_pr_workout(workout: t.Dict) -> t.Dict:
+    """Parses a workout that is a personal best and formats it into a dictionary object
+
+    Args:
+        workout (t.Dict): JSON payload returned from all_workouts for PR workout.
+
+    Returns:
+        t.Dict: Dictionary object containing the necessary fields.
+    """
     pr_dict_values = {"workout_time": format_data_time(workout["created_at"])}
     response = client.get_workout_detail(workout["id"])
     for summary in response["summaries"]:
@@ -20,11 +29,27 @@ def parse_pr_workout(workout):
     return {response["duration"] / 60: pr_dict_values}
 
 
-def format_data_time(epoch_time):
+def format_data_time(epoch_time: int) -> str:
+    """Convert an epoch date to readable format
+
+    Args:
+        epoch_time (int): time in epoch
+
+    Returns:
+        str: human readable date time string.
+    """
     return datetime.datetime.fromtimestamp(epoch_time).strftime("%Y-%m-%d %H:%M:%S")
 
 
-def parse_summary_dict(summary):
+def parse_summary_dict(summary: t.Dict) -> t.Dict:
+    """Parses summary data from workout to retrieve distance, calories, and total output from a workout.
+
+    Args:
+        summary (t.Dict): Summary data from the JSON Peloton API call for a specific workout.
+
+    Returns:
+        t.Dict: The summary data formatted in dictionary format.
+    """
     pr_dict_values = {}
     if summary["display_name"].lower() == "distance":
         miles = summary["value"]
